09a21440f0eefb62c8673ef8b9f1deba1f419285
asmaali2 <asma.maalim7@gmail.com>
2024-02-13 10:50:36 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,40 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
+class Game(db.Model, SerializerMixin):
+    __tablename__ = 'games'
 
-class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
-
-    serialize_rules = ('-baked_goods.bakery',)
+    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    baked_goods = db.relationship('BakedGood', backref='bakery')
-
-    def __repr__(self):
-        return f'<Bakery {self.name}>'
-
-class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
-
-    serialize_rules = ('-bakery.baked_goods',)
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    title = db.Column(db.String, unique=True)
+    genre = db.Column(db.String)
+    platform = db.Column(db.String)
     price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    reviews = db.relationship('Review', backref='game')
 
     def __repr__(self):
-        return f'<Baked Good {self.name}, ${self.price}>'
+        return f'<Game {self.title} for {self.platform}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    serialize_rules = ('-game.reviews', '-user.reviews',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    reviews = db.relationship('Review', backref='user')
--- server/seed.py
+++ server/seed.py
@@ -1,25 +1,110 @@
 #!/usr/bin/env python3
 
-from random import choice as rc
+from random import randint, choice as rc
+
+from faker import Faker
 
 from app import app
-from models import db, Bakery, BakedGood
+from models import db, Game, Review, User
+
+genres = [
+    "Platformer",
+    "Shooter",
+    "Fighting",
+    "Stealth",
+    "Survival",
+    "Rhythm",
+    "Survival Horror",
+    "Metroidvania",
+    "Text-Based",
+    "Visual Novel",
+    "Tile-Matching",
+    "Puzzle",
+    "Action RPG",
+    "MMORPG",
+    "Tactical RPG",
+    "JRPG",
+    "Life Simulator",
+    "Vehicle Simulator",
+    "Tower Defense",
+    "Turn-Based Strategy",
+    "Racing",
+    "Sports",
+    "Party",
+    "Trivia",
+    "Sandbox"
+]
+
+platforms = [
+    "NES",
+    "SNES",
+    "Nintendo 64",
+    "GameCube",
+    "Wii",
+    "Wii U",
+    "Nintendo Switch",
+    "GameBoy",
+    "GameBoy Advance",
+    "Nintendo DS",
+    "Nintendo 3DS",
+    "XBox",
+    "XBox 360",
+    "XBox One",
+    "XBox Series X/S",
+    "PlayStation",
+    "PlayStation 2",
+    "PlayStation 3",
+    "PlayStation 4",
+    "PlayStation 5",
+    "PSP",
+    "PS Vita",
+    "Genesis",
+    "DreamCast",
+    "PC",
+]
+
+fake = Faker()
 
 with app.app_context():
 
-    BakedGood.query.delete()
-    Bakery.query.delete()
-    
-    bakeries = []
-    bakeries.append(Bakery(name='Delightful donuts'));
-    bakeries.append(Bakery(name='Incredible crullers'));
-    db.session.add_all(bakeries)
+    Review.query.delete()
+    User.query.delete()
+    Game.query.delete()
 
-    baked_goods = []
-    baked_goods.append(BakedGood(name='Chocolate dipped donut', price=2.75, bakery=bakeries[0]));
-    baked_goods.append(BakedGood(name='Apple-spice filled donut', price=3.50, bakery=bakeries[0]));
-    baked_goods.append(BakedGood(name='Glazed honey cruller', price=3.25, bakery=bakeries[1]));
-    baked_goods.append(BakedGood(name='Chocolate cruller', price=3.40, bakery=bakeries[1]));
+    users = []
+    for i in range(100):
+        u = User(name=fake.name(),)
+        users.append(u)
 
-    db.session.add_all(baked_goods)
+    db.session.add_all(users)
+
+    games = []
+    for i in range(100):
+        g = Game(
+            title=fake.sentence(),
+            genre=rc(genres),
+            platform=rc(platforms),
+            price=randint(5, 60),
+        )
+        games.append(g)
+
+    db.session.add_all(games)
+
+    reviews = []
+    for u in users:
+        for i in range(randint(1, 10)):
+            r = Review(
+                score=randint(0, 10),
+                comment=fake.sentence(),
+                user=u,
+                game=rc(games))
+            reviews.append(r)
+
+    db.session.add_all(reviews)
+
+    for g in games:
+        r = rc(reviews)
+        g.review = r
+        reviews.remove(r)
+
     db.session.commit()
